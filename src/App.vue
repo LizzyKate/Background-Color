d8e2ad685e109a956888c425d230cd57cc220de0
Drama Queen <dramaqueen@macintoshs-MacBook-Air.local>
2020-04-07 14:29:27 +0100

    My first vue project

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+        <div class="row max-height align-items-center text-center">
+            <div class="col text-center">
+                <button class="btn btn-outline-secondary " v-on:click= "changeBackground()">Click Me!</button>
+            </div> 
+
+        </div>
+               
+    </div>
+    
+</template>
+
+<script>
+export default {
+  
+  data () {
+    return {
+     colors:['red', 'green', 'yellow', 'black', 'pink', 'violet']
+    }
+  },
+  methods:{
+      changeBackground(){
+          let colorIndex = parseInt(Math.random() * this.colors.length);
+          document.body.style.backgroundColor = this.colors[colorIndex]
+          
+      }
+  }
+}
+</script>
+
+<style>
+button {
+    margin-top: 50vh;
+    
+}
+</style>
